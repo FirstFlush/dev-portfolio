711673eed04dc23b6835aac99912b23256f3cbc7
FirstFlush <firstflush@protonmail.com>
2025-03-15 10:42:43 -0700

    Added framework icons to each project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,18 +2,8 @@ import { type Metadata } from 'next'
 import { projects } from './projects'
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/simple-layout'
+import { LinkIcon } from '@/components/icons/icons'
 
-
-function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
 
 export const metadata: Metadata = {
   title: 'Projects',
@@ -32,12 +22,12 @@ export default function Projects() {
       >
         {projects.map((project) => (
           <Card as="li" key={project.name}>
-            <div className='z-10'>
-              <project.logo size={60} />
+            <div className='z-10 flex gap-x-3 items-center'>
+              {project.logos.map((Logo) => (
+                <Logo size={50} />
+              ))}
+              
             </div>
-            {/* <div className="relative z-10 flex  items-center justify-center rounded-full bg-white ring-1 shadow-md shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <project.logo size={50} />
-            </div> */}
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
               <Card.Link href={project.link.href}>{project.name}</Card.Link>
             </h2>
--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -4,7 +4,15 @@ import {
   JavascriptOriginal, 
   PowershellOriginal,
   CsharpOriginal,
-  BashOriginal
+  BashOriginal,
+  PostgresqlOriginal,
+  DjangorestOriginal,
+  DjangoPlain,
+  ReactOriginal,
+  TailwindcssOriginal,
+  RedisOriginal,
+  PlaywrightOriginal,
+  MaterialuiOriginal
 } from 'devicons-react'
 
 interface Project {
@@ -12,7 +20,7 @@ interface Project {
   description: string
   link: { href: string, label: string }
   // logo: React.ComponentType<React.SVGProps<SVGSVGElement>>
-  logo: React.ComponentType<{ size?: string | number; color?: string }>
+  logos: React.ComponentType<{ size?: string | number; color?: string }>[]
 }
 
 
@@ -22,77 +30,77 @@ export const projects: Project[] = [
     description:
       "A full-stack, non-profit project providing real-time resources for vulnerable people in Vancouver. Built with Django REST Framework, Next.js, PostgreSQL (GIS), Redis, Celery, and Twilio for SMS-based assistance.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal, DjangoPlain, RedisOriginal, PostgresqlOriginal],
   },
   {
     name: 'Street Ninja Website',
     description:
       'The official website for the Street Ninja project. It serves as an informational hub for government officials, grant issuers, fundraisers, and NGOs to learn about the initiative. The site features an interactive map and a web-based chat that allows users to simulate the normally SMS-based service in their browser.',
     link: { href: '#', label: 'github.com' },
-    logo: TypescriptOriginal,
+    logos: [TypescriptOriginal, ReactOriginal, TailwindcssOriginal],
   },
   {
     name: "Stegosaurus",
     description:
       "A C# steganography tool that allows users to hide and extract encrypted messages within images using LSB encoding and AES encryption.",
     link: { href: "#", label: "github.com" },
-    logo: CsharpOriginal,
+    logos: [CsharpOriginal],
   },
   {
     name: "Powersweep",
     description:
       "A PowerShell module designed for Windows privilege escalation in Capture The Flag (CTF) scenarios. Automates enumeration of misconfigurations that can lead to privilege escalation.",
     link: { href: "#", label: "github.com" },
-    logo: PowershellOriginal,
+    logos: [PowershellOriginal],
   },
   {
     name: "tire_codes",
     description:
       "A Python package for parsing and decoding automotive tire codes, providing detailed specifications such as tire width, aspect ratio, and speed rating.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal],
   },
   {
     name: "canatax",
     description:
       "A Python tool for quickly calculating Canadian income tax and sales tax rates across provinces, useful for personal finance and business applications.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal],
   },
   {
     name: 'Nikki Racing',
     description:
       'A simple React-based project documenting the build of a Tesla Model 3 racecar. It includes progress updates, modifications, and technical details on the build process.',
     link: { href: '#', label: 'github.com' },
-    logo: JavascriptOriginal,
+    logos: [JavascriptOriginal, ReactOriginal, MaterialuiOriginal],
   },
   {
     name: "Webweaver",
     description:
       "A fully asynchronous web scraper using Playwright and aiohttp, designed for human-like bot behavior, efficient data extraction, and automated website interaction.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal, PlaywrightOriginal, PostgresqlOriginal],
   },
   {
     name: "Tortoise Shell",
     description:
       "A Python tool that provides an interactive shell with Tortoise-ORM model imports, streamlining database interactions for developers using Tortoise ORM.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal],
   },
   {
     name: "django-affiliate-marketing",
     description:
       "A Django plugin for setting up an affiliate marketing program, allowing businesses to track referrals and commissions seamlessly.",
     link: { href: "#", label: "github.com" },
-    logo: PythonOriginal,
+    logos: [PythonOriginal, DjangoPlain],
   },
   {
     name: 'arduino-lib',
     description:
       'A Bash wrapper for Arduino CLI, allowing easier filtering of Arduino packages by name, author, and maintainer.',
     link: { href: '#', label: 'github.com' },
-    logo: BashOriginal,
+    logos: [BashOriginal],
   },
   // {
   //   name: "Unnamed",

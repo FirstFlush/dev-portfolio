ac869e992835e27c24511ebe85da56b29b6a9c09
FirstFlush <firstflush@protonmail.com>
2025-04-30 21:02:42 -0700

    Changed main page blog post formatting from flex-col to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,8 +125,8 @@ export default async function Home() {
       </Container> */}
       {/* <Photos /> */}
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
+        <div className="mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
             {articles.map((article) => (
               <BlogSnippet key={article.slug} article={article} />
             ))}
